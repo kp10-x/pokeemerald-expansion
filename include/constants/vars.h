eb29d62dfa74d9a71275adf2332bb7b8cb902f4e
kp <krishnaprasadv8@gmail.com>
2024-02-19 16:21:41 +0530

    trigger events

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -265,7 +265,7 @@
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
 #define VAR_SELECT_STARTER                               0x40F7 // Select Starter from 9
-#define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
+#define VAR_LITTLEROOT_STATE                             0x40F8 // Littleroot state if collected mon and fought rival
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
 #define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
